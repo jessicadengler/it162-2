dcf8c7389efdd202dd1563c3a8e9b20c0634c3a9
Jessica Dengler <jmdengler@gmail.com>
2021-08-01 20:47:46 -0700

    Jessica

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
        
      <img class="phone" src="images/phone.jpg" alt="Photo of me holding the first crab I ever caught" />
        
-       <h2 class="subheader">A little bit about me!</h2>
+       <h2 class="subheader">A little bit about Jessica!</h2>
        
        <p>Hello! My name is Jessica Dengler and I am in my second quarter of the Web Development Certification program</p>
        <p>Within my Portal Page, you will find links to my projects and the application of what I've learned so far. </p>
